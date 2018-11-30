2d4098a5ab529e17db8cf087b201edd931077939
unknown <fortin_85@hotmail.com>
2018-11-29 21:49:46 -0500

    boiler plate structure setup

---
+++ server/api.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const apiRouter = express.Router();
+
+//Minions Routes
+
+apiRouter.get('/minions', () => {});
+
+
+apiRouter.post('/minions', () => {});
+
+
+apiRouter.get('/minions/:minionId', () => {});
+
+
+apiRouter.put('/minions/:minionId', () => {});
+
+
+apiRouter.delete('minions/:minionId', () => {});
+
+
+//ideas
+
+
+
+//meetings
+
+
+
+
+module.exports = apiRouter;
