084c743d5fdb706c3af8c83548dc4e3c5c1857ac
unknown <fortin_85@hotmail.com>
2018-11-29 23:02:03 -0500

    have both get requests setup for minion/api

--- server/api.js
+++ server/api.js
@@ -1,21 +1,35 @@
 const express = require('express');
 const apiRouter = express.Router();
+const db = require('./db.js');
 
 //Minions Routes
-
-apiRouter.get('/minions', () => {});
-
-
-apiRouter.post('/minions', () => {});
-
-
-apiRouter.get('/minions/:minionId', () => {});
+apiRouter.get('/minions', (req, res, next) => {
+  let minions = db.getAllFromDatabase('minions');
+  res.send(minions);
+});
 
 
-apiRouter.put('/minions/:minionId', () => {});
+apiRouter.post('/minions', (req, res, next) => {
 
 
-apiRouter.delete('minions/:minionId', () => {});
+});
+
+
+apiRouter.get('/minions/:minionId', (req, res, next) => {
+    let minion = db.getFromDatabaseById('minions', req.params.minionId);
+    console.log('Minion Id Located Here');
+    if (minion) {
+      res.send(minion);
+    } else {
+      res.status(404).send();
+    }
+});
+
+
+apiRouter.put('/minions/:minionId', (req, res, next) => {});
+
+
+apiRouter.delete('minions/:minionId', (req, res, next) => {});
 
 
 //ideas
