927a8a1d66755c66eb8457c7d0e5e7fb5fa72789
unknown <fortin_85@hotmail.com>
2018-11-30 23:02:13 -0500

    have the get/put/post/deletes setup for minions

--- server/api.js
+++ server/api.js
@@ -10,26 +10,42 @@ apiRouter.get('/minions', (req, res, next) => {
 
 
 apiRouter.post('/minions', (req, res, next) => {
-
-
+  const newMinion = db.addToDatabase('minions', req.body);
+  res.status(201).send(newMinion);
 });
 
 
 apiRouter.get('/minions/:minionId', (req, res, next) => {
-    let minion = db.getFromDatabaseById('minions', req.params.minionId);
-    console.log('Minion Id Located Here');
-    if (minion) {
-      res.send(minion);
-    } else {
-      res.status(404).send();
-    }
+  let minion = db.getFromDatabaseById('minions', req.params.minionId);
+  if (minion) {
+    res.send(minion);
+  } else {
+    res.status(404).send();
+  }
 });
 
 
-apiRouter.put('/minions/:minionId', (req, res, next) => {});
+apiRouter.put('/minions/:minionId', (req, res, next) => {
+  let minionId =  db.getFromDatabaseById('minions', req.params.minionId);
+  if (minionId) {
+    const minionUpdated = db.updateInstanceInDatabase('minions', req.body);
+    res.send(minionUpdated);
+  } else {
+    res.status(404).send();
+  }
+});
 
 
-apiRouter.delete('minions/:minionId', (req, res, next) => {});
+
+apiRouter.delete('/minions/:minionId', (req,res,next) =>{
+  let minion =  db.getFromDatabaseById('minions', req.params.minionId);
+  if(minion) {
+    db.deleteFromDatabasebyId('minions', minion.id);
+    res.status(204).send();
+  } else {
+    res.status(404).send();
+  }
+});
 
 
 //ideas
